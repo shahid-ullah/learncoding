9fd3d5e392303a5980f570ef592e84550e030333
shahid <shahid.ice.nstu@gmail.com>
2020-11-24 13:14:32 +0600

    Registered article app urls.py

--- learncoding/urls.py
+++ learncoding/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    path('', include('article.urls')),
         ]
 
 admin.site.index_title = "Welcome to  learncoding Administration"
