2666364e0d9c357b186cfb7b8e38ae1e14c018b2
shahid <shahid.ice.nstu@gmail.com>
2020-11-24 13:15:44 +0600

    attach url for ListArticleView, DetailArticleView, about view

---
+++ article/urls.py
@@ -0,0 +1,11 @@
+# article/urls.py
+
+from django.urls import path
+
+from .views import DetailArticleView, ListArticleView, about
+
+urlpatterns = [
+    path('', ListArticleView.as_view(), name='article_list'),
+    path('detail/<int:pk>/', DetailArticleView.as_view(), name='article_detail'),
+    path('about/', about, name='about'),
+    ]
